5229a2dc95e35e61bd9dc966cb8f8b7e314d9fd4
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-08-14 14:20:48 +0000

    Added custom bash script to test Compose Application

---
+++ tests/end-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+frontend="localhost:8080" 
+backend="localhost:9000" 
+
+frontend_keyword="<!DOCTYPE html>" # enter the keyword for test content
+backend_keyword="[]"
+
+if curl -s "$frontend" | grep "$frontend_keyword"
+then
+    # if the keyword is in the content
+    echo "The frontend is working"
+    if curl -s "$backend" | grep "$frontend_keyword"
+    then
+        echo "Everything works"
+        exit 0
+    else
+        echo "Error - backend is wrong"
+        exit 1
+    fi
+else
+    echo "Error - frontend is wrong"
+    exit 1
+fi
